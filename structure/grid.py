a81fd82a04c7c92ba46c49d9710fa88076e79295
ClaudiusLicinius <claudiuslicinius@gmail.com>
2020-06-18 23:31:05 +0200

    comm

--- structure/grid.py
+++ structure/grid.py
@@ -1,5 +1,6 @@
-#gegenüberliegende Ränder sollen verbunden sein
-#die Felder sind inhaltsleer, sind enthalten bloß einen place, der die lokal-Daten speichert
+# gegenüberliegende Ränder sollen verbunden sein
+# die Felder sind inhaltsleer, sind enthalten bloß einen place
+
 
 class Grid:
     def __init__(self, world, height, width):
