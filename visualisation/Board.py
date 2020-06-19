6f3639ab26b6046d51e9500fe432f75635b90962
Jan <jan.hofmann@uni-oldenburg.de>
2020-06-19 21:15:37 +0200

    right side offsett

--- visualisation/Board.py
+++ visualisation/Board.py
@@ -17,18 +17,11 @@ class Board:
         self.window = window
 
         for x in range(world.grid.width):
-            # window.columnconfigure((x, x + len(window.winfo_children())),
-            #                        weight=1, minsize=50)
-            # window.rowconfigure((x, x + len(window.winfo_children())),
-            #                     weight=1, minsize=50)
-            # window.columnconfigure(x, weight=1, minsize=50)
-            # window.rowconfigure(x, weight=1, minsize=50)
             for y in range(world.grid.height):
                 help_x = x * 2
                 if y % 2 == 0:
-                    if x == 0:
-                        self.add_offset_row(x, y)
                     help_x += 1
+
                 cell = PlaceFrame(master=self.window, relief=tk.RAISED,
                                   borderwidth=5)
                 self.world.grid.places[x][y].add_observer(cell)
@@ -36,14 +29,21 @@ class Board:
                 cell.grid(row=y * 2, column=help_x, padx=5, pady=5,
                           sticky="nesw", rowspan=2, columnspan=2)
 
-        for col_num in range(window.grid_size()[1]):
+        for y in range(world.grid.height):
+            if y % 2 == 1:
+                cell = tk.Frame(master=self.window)
+                cell.grid(row=y * 2, column=world.grid.width * 2,
+                          sticky="nesw")
+
+        for col_num in range(window.grid_size()[1] + 1):
             window.columnconfigure(col_num, weight=1, minsize=50)
         for row_num in range(window.grid_size()[0]):
             window.rowconfigure(row_num, weight=1, minsize=50)
 
     def add_offset_row(self, x, y):
-        cell = tk.Frame(master=self.window)
-        cell.grid(row=y * 2, column=x, sticky="nesw", rowspan=2, columnspan=1)
+        cell = tk.Frame(master=self.window, bg='blue')
+        cell.grid(row=y * 2, column=2 * x, sticky="nesw", rowspan=2,
+                  columnspan=2, padx=5, pady=5)
 
     def step(self):
         pass
