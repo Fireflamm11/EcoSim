04e2b8f647e03bb5bd8ee68d6103a8aa6b2fd45a
Jan <jan.hofmann@uni-oldenburg.de>
2020-06-20 17:10:24 +0200

    Remove Thread

--- structure/World.py
+++ structure/World.py
@@ -4,10 +4,9 @@ from structure.GridFactory import GridFactory
 import time
 
 
-class World(Thread):
+class World():
 
     def __init__(self, height, width, world_type='empty', start_date=0):
-        super().__init__()
         self.date = start_date
         self.type = world_type
         self.grid = GridFactory.generate_places(self, self.type, height, width)
