c36ad0c9661a7ff930bbfa22254ff0b1c4572a54
Jan <jan.hofmann@uni-oldenburg.de>
2020-06-25 18:35:24 +0200

    fixes

--- implementation/settlements/Village.py
+++ implementation/settlements/Village.py
@@ -5,7 +5,7 @@ from structure.Settlement import Settlement
 
 
 class Village(Settlement):
-    def __init__(self, place, start_pops, strata):
+    def __init__(self, place, start_pops, strata='communal'):
         super().__init__(place)
         self.strata = strata
         self.arable_land = np.random.random() * 100 + 10
