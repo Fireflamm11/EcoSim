2b234b9eef181272b2291b3213ff89253e48a564
Jan <jan.hofmann@uni-oldenburg.de>
2020-06-25 20:57:04 +0200

    fixup merge+ some comments on why

--- implementation/settlements/Communal.py
+++ implementation/settlements/Communal.py
@@ -9,6 +9,7 @@ class Communal(AgentStrata):
     @classmethod
     def production(cls, agent, **kwargs):
         for _ in range(agent.village.place.soil_quality):
+            # TODO why iterate over soil_quality
             if len(agent.village.pops) <= agent.village.arable_land:
                 agent.village.food += len(agent.village.pops)
             else:
@@ -19,9 +20,10 @@ class Communal(AgentStrata):
         agent.village.place.changed_values['starving'] = True
         new_pops = 0
         agent.village.food -= 2 * len(agent.village.pops)
-        starving = int(-agent.village.food)
-        if starving > len(agent.village.pops):
-            starving = len(agent.village.pops)
+        agent.starving = int(-agent.village.food)
+        # TODO why negative? And why class variable(its get overwritten)
+        if agent.starving > len(agent.village.pops):
+            agent.starving = len(agent.village.pops)
         if agent.village.food < 0:
             for pop in agent.village.pops:
                 pop.health -= 50
@@ -32,20 +34,14 @@ class Communal(AgentStrata):
                 pop.health += 10
                 new_pops += 1
                 agent.village.food = 0
-        cls.migrate_pop(agent, starving)
+        # cls.migrate_pop(agent, agent.starving)
         agent.village.food = 0
         cls.grow_pop(agent, new_pops)
 
     @classmethod
     def pop_development(cls, agent, **kwargs):
-        for pop in agent.village.pops:
-            pop.age += 1
-            if pop.age >= 30:
-                pop.health -= 10
-            if pop.age >= 40:
-                pop.health -= 10
-            if pop.health <= 0:
-                cls.kill_pop(agent, pop)
+        cls.ageing(agent)
+        cls.migrate_pop(agent)
 
     @classmethod
     def kill_pop(cls, agent, pop):
@@ -67,7 +63,7 @@ class Communal(AgentStrata):
     @classmethod
     def grow_pop(cls, agent, new_pops):
         for _ in range(new_pops):
-            if np.random.random() * 100 <= 33:
+            if np.random.random() * 100 <= 3:
                 PopFactory.generate_pops(agent.village, job='farmer',
                                          food_need=2)
         if agent.village.place.changed_values.get('pops') is not None:
@@ -78,14 +74,26 @@ class Communal(AgentStrata):
                 agent.village.pops)
 
     @classmethod
-    def migrate_pop(cls, agent, starving):
+    def ageing(cls, agent):
+        for pop in agent.village.pops:
+            pop.age += 1
+            if pop.age >= 30:
+                pop.health -= 10
+            if pop.age >= 40:
+                pop.health -= 10
+            if pop.health <= 0:
+                cls.kill_pop(agent, pop)
+
+    @classmethod
+    def migrate_pop(cls, agent):
         moving = []
-        for pop in range(starving):
+        if agent.starving > len(agent.village.pops):
+            agent.starving = len(agent.village.pops)
+        for pop in range(agent.starving):
             moving.append(agent.village.pops[pop])
         agent.village.pops = [x for x in agent.village.pops if x not in moving]
         for direction in agent.village.place.directions:
             neighbor = agent.village.place.neighbors[direction]
-
             for settlement in neighbor.settlements:
                 if settlement.arable_land - len(settlement.pops) > 0:
                     settlement.pops.extend(moving)
